bd2497bf5ed2181c3a94ad1b0348ac25d41b2fee
DragonMess <kmylop@hotmail.com>
2022-04-07 10:38:58 -0400

    input validate

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,55 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsTouched, setEnteredNameIsTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameIsTouched;
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid(true);
+  }
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const nameInputBlurHandler = () => {
+    setEnteredNameIsTouched(true);
+  };
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    setEnteredNameIsTouched(true);
+
+    if (!enteredNameIsValid) {
+      return;
+    }
+    console.log(enteredName);
+    setEnteredName("");
+    setEnteredNameIsTouched(false);
+  };
+
+  const nameInputClasses = nameInputIsInvalid ? "form-control invalid" : "form-control";
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+          onBlur={nameInputBlurHandler}
+        />
+        {nameInputIsInvalid && <p className="error-text">Name is not be empty</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
